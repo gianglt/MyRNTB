618162fa67664a32b493a4265f69ac0e2641336c
Giang Lam <gianglt@gmail.com>
2022-05-10 08:30:17 +0700

    Initialize newline

---
+++ shared/services/search.ts
@@ -0,0 +1,36 @@
+import API from './core/api';
+import Config from '../../src/config';
+
+import searchResult from '../mock/amazon/search';
+import productResult from '../mock/amazon/product';
+import { SearchResponse } from '../redux/types/search/ISearchResponse';
+import { ProductResponse } from '../redux/types/search/IProductResponse';
+
+export const SearchService = {
+  fetch: (searchTerm: string): Promise<SearchResponse> => {
+    // return API.get(
+    //   Config.APIs.endpoints.search + encodeURI(searchTerm),
+    //   { api_key: Config.APIs.api_key }
+    // ).then((res) => res.json())
+    // .catch((err) => {
+    //   console.log('>>> err:', err);
+    //   return Promise.resolve({ error: err });
+    // });
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(searchResult), 1000);
+    });
+  },
+  product: (productId: string): Promise<ProductResponse> => {
+    // return API.get(
+    //   Config.APIs.endpoints.product + encodeURI(productId),
+    //   { api_key: Config.APIs.api_key }
+    // ).then((res) => res.json())
+    // .catch((err) => {
+    //   console.log('>>> err:', err);
+    //   return Promise.resolve({ error: err });
+    // });
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(productResult), 1000);
+    })
+  }
+};
